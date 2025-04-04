88dff659310fedec79ca1a36a0a9ba5aaa480269
MaxLaPeuff <maxaraye18@gmail.com>
2025-04-04 14:35:37 +0100

    ajout des migrations

--- build.sh
+++ build.sh
@@ -5,11 +5,17 @@ set -o errexit
 # Installation des dépendances
 pip install -r requirements.txt
 
+# Créer les dossiers nécessaires
+mkdir -p ministore/static ministore/media
+
 # Entrer dans le répertoire du projet Django
 cd ministore
 
-# Collecter les fichiers statiques
-python manage.py collectstatic --no-input
+# Créer les migrations si elles n'existent pas
+python manage.py makemigrations
 
 # Appliquer les migrations
-python manage.py migrate
+python manage.py migrate --noinput
+
+# Collecter les fichiers statiques
+python manage.py collectstatic --noinput
